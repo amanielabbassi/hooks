e4cf9c2eb349bd0c568d4e1cb3f348b70f2d4d1d
amanielabbassi <lyemnaabassi@gmail.com>
2021-09-16 17:20:17 +0100

    movie-app

---
+++ src/components/Movie.js
@@ -0,0 +1,31 @@
+import { Card } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Rating from '@material-ui/lab/Rating';
+
+
+
+
+const Movie = ({movie,ratingChanged}) => {
+  return (
+    <div className='movie' >
+      {movie.map( (movie)=> (
+        
+           
+        <Card   className='row' style={{ width: '18rem' }}>
+            <div >
+            <Rating name="read-only" value={movie.rating} readOnly />
+  <Card.Img className='img' variant="top" src={movie.Poster} /></div>
+  <Card.Body>
+    <p>{movie.Title}({movie.Year}) </p>
+    
+    
+  </Card.Body>
+</Card>
+
+
+      ))}
+    </div>
+  );
+};
+
+export default Movie;
